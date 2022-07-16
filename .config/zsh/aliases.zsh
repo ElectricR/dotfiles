653ab4dbc6ac48cac7aeb4d726136ae1b850018e
ElectricR <chulkovroma@mail.ru>
2022-07-16 20:38:02 +0300

    Added dvorak aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -13,3 +13,6 @@ alias tms="python3 ~/projects/TMS/TMS2.py"
 alias headphones="bluetoothctl power on && bluetoothctl connect 00:AD:D5:5A:5B:10"
 
 alias xclip="xclip -selection c"
+
+alias dvpon="setxkbmap -model pc104 -layout us,ru -variant dvp, -option grp:alt_shift_toggle"
+alias dvpoff="setxkbmap -model pc104 -layout us,ru -option grp:alt_shift_toggle"
