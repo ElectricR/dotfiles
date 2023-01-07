196d936c49bc720b30a7ef1541e3fd7eb8cff412
ElectricR <chulkovroma@mail.ru>
2023-01-07 20:23:46 +0300

    Added chsh for android in yadm bootstrap

--- .config/yadm/android.py
+++ .config/yadm/android.py
@@ -8,6 +8,11 @@ ANDROID_PACKAGES = ["wget", "tmux", "taskwarrior", "tree", "zsh"]
 # Steps
 #####################
 
+def configure_shell():
+    retcode = os.system("chsh -s zsh")
+    return 1 if retcode else 0
+
+
 def install_packages() -> int:
     package_manager = "pkg install"
     retcode = os.system(f"{package_manager} " + ' '.join(ANDROID_PACKAGES))
@@ -20,5 +25,5 @@ def install_packages() -> int:
 def bootstrap():
     def zsh_prompt_icon():
         return common.zsh_prompt_icon('PROMPT_HOST_SPECIFIC_ICON="A "')
-    steps = [install_packages, common.zsh_bd, zsh_prompt_icon]
+    steps = [install_packages, configure_shell, common.zsh_bd, zsh_prompt_icon]
     common.bootstrap(steps)
