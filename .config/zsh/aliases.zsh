79b367d71d5dcc0bd9501fbde0c95b1773e579e5
ElectricR <chulkovroma@mail.ru>
2023-06-14 12:17:55 +0300

    Added kitty reload alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -35,3 +35,5 @@ alias dvpoff="setxkbmap -model pc104 -layout us,ru -option grp:alt_shift_toggle"
 
 alias ssh="TERM=xterm ssh"
 alias pssh="TERM=xterm pssh"
+
+alias kitty-reload='kill -SIGUSR1 $(pgrep kitty)'
