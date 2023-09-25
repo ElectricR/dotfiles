a42a234cfc10623a5c1be6942d7ba183fa162844
ElectricR <chulkovroma@mail.ru>
2023-09-25 09:07:24 +0300

    Fixed highlighting in kitty pager

---
+++ .config/kitty/pager.sh
@@ -0,0 +1,7 @@
+source /home/er/.config/zsh/palette.zsh
+nvim -n \
+    -c "set nonumber nolist norelativenumber showtabline=0 foldcolumn=0" \
+    -c "autocmd TermOpen * normal G" \
+    -c "map <silent> q :qa!<CR>" \
+    -c "silent write! /tmp/kitty_scrollback_buffer" \
+    -c "te head -q -n-1 /tmp/kitty_scrollback_buffer; rm /tmp/kitty_scrollback_buffer; cat"
