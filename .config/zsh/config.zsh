c86827880958efc1390563148dd2a41e97122b82
ElectricR <chulkovroma@mail.ru>
2024-02-23 14:56:13 +0300

    Added brackets and quotes text objects to zsh vim mode

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -55,6 +55,20 @@ zstyle ':completion:*' menu select
 compinit
 _comp_options+=(globdots)		# Include hidden files.
 
+# Vim brackets and quotes text objects
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for km in viopp visual; do
+  bindkey -M $km -- '-' vi-up-line-or-history
+  for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
+    bindkey -M $km $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $km $c select-bracketed
+  done
+done
+
 # Autosuggestions
 source $HOME/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
