2b2bb093523708479223dc0aab9db4712e4f4e8e
ElectricR <chulkovroma@mail.ru>
2024-03-09 14:53:26 +0300

    Fixed TMPDIR for neomutt for android

--- .config/neomutt/berry_port_map.sh
+++ .config/neomutt/berry_port_map.sh
@@ -1,6 +1,14 @@
+function __get_tmp_dir() {
+    if [[ -n $TMPDIR ]]; then
+        echo ${TMPDIR::-4}
+    else
+        echo ""
+    fi
+}
+
 while true; do
-    nc -l localhost 44445 > ${TMPDIR}/tmp/kek
-    xdg-open ${TMPDIR}/tmp/kek
+    nc -l localhost 44445 > $(__get_tmp_dir)/tmp/kek
+    xdg-open $(__get_tmp_dir)/tmp/kek
     sleep 1;
 done
 
