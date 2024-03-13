9c3af4557c1c9b860bea192dd2d20fbdf9410312
ElectricR <chulkovroma@mail.ru>
2024-03-14 01:04:02 +0300

    Neomutt temp dir name change

--- .config/neomutt/berry_port_map.sh
+++ .config/neomutt/berry_port_map.sh
@@ -7,8 +7,9 @@ function __get_tmp_dir() {
 }
 
 while true; do
-    nc -l localhost 44445 > $(__get_tmp_dir)/tmp/kek
-    xdg-open $(__get_tmp_dir)/tmp/kek
+    nc -l localhost 44445 > $(__get_tmp_dir)/tmp/neomutt_berry_pre
+    cp $(__get_tmp_dir)/tmp/neomutt_berry_pre $(__get_tmp_dir)/tmp/neomutt_berry
+    xdg-open $(__get_tmp_dir)/tmp/neomutt_berry
     sleep 1;
 done
 
